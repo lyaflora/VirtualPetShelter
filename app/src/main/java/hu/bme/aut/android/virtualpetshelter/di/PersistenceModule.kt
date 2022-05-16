d05830a698428299e8c2ab57909bf54bdef67d69
lyaflora <petrok.flora@gmail.com>
2022-05-16 06:16:19 +0200

    Update Hilt persistence and repository modules

--- app/src/main/java/hu/bme/aut/android/virtualpetshelter/di/PersistenceModule.kt
+++ app/src/main/java/hu/bme/aut/android/virtualpetshelter/di/PersistenceModule.kt
@@ -1,10 +1,35 @@
 package hu.bme.aut.android.virtualpetshelter.di
 
+import android.app.Application
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import hu.bme.aut.android.virtualpetshelter.persistence.PetDao
+import hu.bme.aut.android.virtualpetshelter.persistence.PetDatabase
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class PersistenceModule {
+object PersistenceModule {
+
+    @Provides
+    @Singleton
+    fun providePetDatabase(application: Application): PetDatabase {
+        return Room
+            .databaseBuilder(
+                application,
+                PetDatabase::class.java,
+                "pets.db"
+            )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePetDao(petDatabase: PetDatabase): PetDao {
+        return petDatabase.petDao()
+    }
 }
